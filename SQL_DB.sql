1b979d1ec0a46f99a66a2da41c5c2d77f8b4d895
jstrieb/github-stats <vadnetvadnet@ukr.net>
2025-01-15 18:17:50 +0200

    Update SQL_DB.sql

--- SQL_DB.sql
+++ SQL_DB.sql
@@ -52,3 +52,106 @@ VALUES ('Allen Raymond', 'nulla.ante@vestibul.co.uk', '(992) 914-3792', 0, 1),
        ('Kennedy Lane', 'mattis.Cras@nonenimMauris.net', '(542) 451-7038', 1, 2),
        ('Wylie Pope', 'est@utquamvel.net', '(692) 802-2949', 0, 2),
        ('Cyrus Jackson', 'nibh@semsempererat.com', '(501) 472-5218', 0, null);
+
+SELECT * FROM contacts
+
+SELECT name, email FROM contacts ORDER BY name
+
+SELECT name, email
+FROM contacts
+WHERE favorite = true
+ORDER BY name
+
+SELECT name, email
+FROM users
+WHERE age IN(20, 30, 40)
+ORDER BY name
+
+SELECT name, email, age
+FROM users
+WHERE age BETWEEN 30 AND 40
+ORDER BY name
+
+SELECT name, email
+FROM contacts
+WHERE name LIKE '%L%'
+ORDER BY name
+
+SELECT name, email, age
+FROM users
+WHERE age NOT BETWEEN 30 AND 40
+ORDER BY name
+
+SELECT min(age) as minAge
+FROM users
+
+SELECT avg(age) as averageAge
+FROM users
+
+SELECT COUNT(user_id) as total_contacts, user_id
+FROM contacts
+GROUP BY user_id
+
+SELECT *
+FROM contacts
+WHERE user_id IN (SELECT id
+    FROM users
+    WHERE age < 30)
+
+SELECT id, name as fullName, phone as mobile
+FROM contacts
+
+SELECT u.id, u.name, u.email, g.name AS gender
+FROM users AS u
+INNER JOIN genders AS g ON g.id = u.gender_id
+
+SELECT c.id, c.name, c.email, u.name AS owner
+FROM contacts AS c
+JOIN users AS u ON u.id = c.user_id
+
+UPDATE contacts SET user_id = 3 WHERE id = 5;
+
+DELETE FROM contacts WHERE id = 4;
+
+TRUNCATE TABLE contacts;
+
+DROP TABLE contacts;
+
+ALTER TABLE table_name CHANGE column_a column_b INTEGER;
+
+ALTER TABLE table_name MODIFY column_b BIGINT NOT NULL;
+
+ALTER TABLE table_name ADD column_c FLOAT;
+
+ALTER TABLE table_name ADD column_d VARCHAR(30) AFTER column_c;
+
+ALTER TABLE table_name DROP COLUMN column_c;
+
+ALTER TABLE table_name ADD INDEX index_name_ix (column_b);
+
+CREATE INDEX fullname_ix ON users (fullname);
+
+DROP INDEX table1_price_ix ON table1;
+
+ALTER TABLE table1 ADD CONSTRAINT table1_id_pk PRIMARY KEY (id);
+
+ALTER TABLE table1 DROP PRIMARY KEY;
+
+CREATE TABLE persons (
+    id INT PRIMARY KEY,
+    email CHAR(50) NOT NULL,
+    fullName varchar(100) NULL,
+    CONSTRAINT persons_email_un UNIQUE KEY (email)
+);
+
+
+
+
+
+
+
+
+
+
+
+
